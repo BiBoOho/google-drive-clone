d22ab9c1e4e4e15477b8c73c65d89d4128794b15
BiBoDeveloper <151799383+BiBoDeveloper@users.noreply.github.com>
2024-01-19 08:23:14 +0700

    add data

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -60,6 +60,16 @@ export default {
           id: 9,
           Foldername: 'Folder09',
           ParentFolder: '7',
+        },
+        {
+          id: 10,
+          Foldername: 'Folder19',
+          ParentFolder: '2',
+        },
+        {
+          id: 11,
+          Foldername: 'Folder12',
+          ParentFolder: '2',
         }
       ];
 
